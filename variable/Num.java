23a579cc240b17693420b4e8b60e8bd489b71a45
anallya <anallyaullas20@gmail.com>
2025-03-12 14:35:14 +0530

    its me anallya

---
+++ variable/Num.java
@@ -0,0 +1,12 @@
+public class Num{
+    /*
+    1)java filename
+    2)using variable
+    */
+ public static void main(String args[])
+{
+    int numOne=10; //this int valu
+    System.out.println(numOne);
+    
+    }
+}
