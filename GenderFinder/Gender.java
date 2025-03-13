5eb95405012d688c6656d1c2c3630b9c6aad55ce
anallya <anallyaullas20@gmail.com>
2025-03-13 11:44:00 +0530

    it is male

---
+++ GenderFinder/Gender.java
@@ -0,0 +1,12 @@
+public class Gender{
+    public static void main(String args[]){
+        char gender ='m';
+        if(gender=='m')
+        {
+            System.out.println("it is male");
+        }
+        else
+            System.out.println("it is female");
+        }
+        
+    }
