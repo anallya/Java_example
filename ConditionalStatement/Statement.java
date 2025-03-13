ce9934f0068579cc4e0e24ffc840d337e8c19e56
anallya <anallyaullas20@gmail.com>
2025-03-13 10:55:06 +0530

    this my statement

---
+++ ConditionalStatement/Statement.java
@@ -0,0 +1,14 @@
+public class Statement{
+    public static void main(String args[]){
+        if(10>=2){
+
+
+            System.out.println(true);
+        }
+        if(10<=2)
+        {
+            System.out.println(false);
+
+        }
+        }
+    }
