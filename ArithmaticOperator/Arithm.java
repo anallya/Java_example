55f3450d25d7f18df2bc7d873cd838707ed80bb6
anallya <anallyaullas20@gmail.com>
2025-03-13 10:35:28 +0530

    my arithm opertor

---
+++ ArithmaticOperator/Arithm.java
@@ -0,0 +1,14 @@
+public class Arithm{
+    public static void main(String args[])
+    {
+        System.out.println(10+2);
+        System.out.println(10-2);
+        System.out.println(10/2);
+        System.out.println(10%2);
+    
+    }
+
+}
+
+
+    
