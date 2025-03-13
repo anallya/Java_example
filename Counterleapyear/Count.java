dfbf20bd2b788e0b94cb4f4b4fa360f31d39eae3
anallya <anallyaullas20@gmail.com>
2025-03-13 15:09:16 +0530

    counter leapyear

---
+++ Counterleapyear/Count.java
@@ -0,0 +1,13 @@
+public class Count{
+    public static void main(String args[]){
+        int count=0;
+        for(int i =2020;i<2030;i++){
+            if(i%4==0)
+        {
+            count =count+i;
+
+        }
+        System.out.println(count);
+    }
+}
+}
