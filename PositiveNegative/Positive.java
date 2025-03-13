2287b9b2b7904f62e9c75e9df267078e06e3b0d2
anallya <anallyaullas20@gmail.com>
2025-03-13 11:27:49 +0530

    this is positive commit

---
+++ PositiveNegative/Positive.java
@@ -0,0 +1,12 @@
+public class Positive {
+    public static void main(String args[]){
+        int x = 35;
+        if (x>=0)
+        {
+            System.out.println("positive");
+            }
+            else
+            System.out.println("negative");
+    }
+    }
+    
