cccfa3a98b10ade11dc384582ba88c653ad294e9
anallya <anallyaullas20@gmail.com>
2025-03-13 14:49:04 +0530

    its Leapyear

---
+++ Leapyear/Leapyear.java
@@ -0,0 +1,15 @@
+public class Leapyear{
+    public static void main(String args[]){
+        int i =2020;
+        if(i%4==0)
+        {
+            System.out.println("leap year");
+
+        }
+        else
+        {
+            System.out.println("not a leap year");
+        }
+
+        }
+    }
