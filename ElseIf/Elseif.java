3f98923ee995efc2e76aa522c838b244d750ac1b
anallya <anallyaullas20@gmail.com>
2025-03-13 12:10:48 +0530

    elseif else

---
+++ ElseIf/Elseif.java
@@ -0,0 +1,16 @@
+public class Elseif {
+    public static void main(String args[]){
+        
+        int num = 0;
+        if(num>=-1) {
+System.out.println("positive");
+        }else if(num == 0){
+System.out.println("zero");
+        }
+        else{
+        System.out.println("negative");
+    }
+}
+
+}
+    
