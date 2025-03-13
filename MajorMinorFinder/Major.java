65930156207204ddad96c2c0c6953fec2ad1bebf
anallya <anallyaullas20@gmail.com>
2025-03-13 11:18:23 +0530

    i am major

---
+++ MajorMinorFinder/Major.java
@@ -0,0 +1,12 @@
+public class Major{
+    public static void main(String args[]){
+        int x = 18;
+        if (x>=18)
+        {
+            System.out.println("major");
+            }
+            else
+            System.out.println("minor");
+    }
+    }
+    
