a2da20a49cedfbed159de9a5f115a1d974692fec
anallya <anallyaullas20@gmail.com>
2025-03-14 11:59:38 +0530

    splited the words

---
+++ Split/Split.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Split{
+    public static void main(String args[]){
+        String name="hello ,java";
+        System.out.println(name.length());
+        
+
+    }
+    
+}
