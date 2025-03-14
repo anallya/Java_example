52299ddaa344d9b24907e2ea12300d95b8ac973f
anallya <anallyaullas20@gmail.com>
2025-03-14 13:48:47 +0530

    mult level

---
+++ Multlevel/Mult.java
@@ -0,0 +1,24 @@
+class Parent{
+    public void add(){
+        System.out.println("hello world");
+    }
+}
+class Child extends Parent
+{
+    void display(){
+        add();
+    }
+}
+class Third extends Child{
+    void helper(){
+        display();
+
+    }
+}
+ public class Mult{
+    public static void main(String args[]){
+Third obj = new Third();
+    obj.helper();
+    } 
+    }
+
