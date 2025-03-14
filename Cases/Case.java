cbb616bcdb92635ec427b827e3fc3fc6591665e7
anallya <anallyaullas20@gmail.com>
2025-03-14 12:13:56 +0530

    its my case

---
+++ Cases/Case.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Case{
+    public static void main(String args[]){
+        String name="hello,world";
+        System.out.println(name.indexOf(","));
+        }
+
+    }
