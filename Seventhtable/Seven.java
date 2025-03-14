f6f6e0ca866c899da8675a7ecc74ef77e71118eb
anallya <anallyaullas20@gmail.com>
2025-03-14 10:36:04 +0530

    my seventh table

---
+++ Seventhtable/Seven.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Seven{
+    public static void main(String args[])
+    {
+        int i =0;
+        int x =0;
+        while(i<70)
+        {
+            i=i+7;
+            x=x+1;
+            System.out.println(x+"x7="+i);
+        }
+    }
+}
