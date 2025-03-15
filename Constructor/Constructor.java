fbdb52e3047f1a05125dfdfd011048b783c9fb02
anallya <anallyaullas20@gmail.com>
2025-03-15 11:05:14 +0530

    it is constructor

---
+++ Constructor/Constructor.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Constructor{
+    Constructor(){
+        System.out.println("anallya");
+    }
+    public static void main(String args[]){
+        Constructor obj = new Constructor();
+
+    }
+}
