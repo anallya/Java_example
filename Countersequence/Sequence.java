79dff9ded1aec91248afe5f0cea6312083f41e2e
anallya <anallyaullas20@gmail.com>
2025-03-15 10:24:36 +0530

    counter sequence

---
+++ Countersequence/Sequence.java
@@ -0,0 +1,12 @@
+public class Sequence{
+    public static void main(String args[]){
+        for(int i=2020;i<2023;i++)
+        {
+            if(i%4==0){
+            }
+            System.out.println(i);
+        }
+
+        
+    }
+}
