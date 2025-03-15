c6ab1ee38315a96683e180d181d87da164cabd0a
anallya <anallyaullas20@gmail.com>
2025-03-15 09:44:30 +0530

    sum of dgit

---
+++ Sumofdigit/Sum.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Sum{
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter the n value");
+        int n=input.nextInt();
+        int a[] = new int [n];
+        int i=0;
+        int j=0;
+        while(i<n){
+            System.out.println("enter the value");
+            a[i]=input.nextInt();
+            i=i+1;
+        }
+        j=0;
+        int sum =0;
+
+        while(j<n){
+            sum=sum+a[j];
+            j =j+1;
+        }
+
+System.out.println("Addition:"+(sum));
+        
+        }
+        }
+    
