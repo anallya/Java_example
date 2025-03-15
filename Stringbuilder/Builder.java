4d33d17b291518d1c2a409241047540637f191b2
anallya <anallyaullas20@gmail.com>
2025-03-15 10:52:57 +0530

    string builder

---
+++ Stringbuilder/Builder.java
@@ -0,0 +1,7 @@
+public class Builder{
+    public static void main(String args[]){
+        String S1 ="athulya";
+        String S2 ="anallya";
+        System.out.println("name:"+(S1+S2));
+    }
+}
